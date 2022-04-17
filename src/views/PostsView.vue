c17ca7195e4ec69cb372e23db5866aadfd1aa186
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-17 15:45:57 -0300

    Compositon API - Route (useRoute)

---
+++ src/views/PostsView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="posts">
+
+    <h1>Posts</h1>
+
+    <ul>
+      <li>
+        <router-link to="/postDetail/id1">Post 1</router-link>
+      </li>
+      <li>
+        <router-link to="/postDetail/id2">Post 2</router-link>
+      </li>
+      <li>
+        <router-link to="/postDetail/id3">Post 3</router-link>
+      </li>
+    </ul>
+
+    <textarea v-autofocus></textarea>
+
+  </div>
+</template>
+
+<script setup>
+/**
+ * Imports
+ */
+import {vAutofocus} from "../directives/vAutofocus";
+</script>
+
+<style scoped>
+ul {
+  margin-bottom: 30px;
+}
+</style>
