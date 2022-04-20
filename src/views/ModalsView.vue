46d85904ced61f15dca1cf98ee52afc66e2ad366
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-20 17:22:17 -0300

    Compositon API - Teleport

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,13 +2,15 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <div class="modal" v-if="showModal">
-      <h1>This is a modal</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus, enim est eum excepturi explicabo
-        harum, ipsam iste iusto laudantium modi officia perspiciatis quae rem repudiandae vel velit voluptate.
-        Dolorem!</p>
-      <button @click="showModal = false">Hide modal</button>
-    </div>
+    <teleport to="body">
+      <div class="modal" v-if="showModal">
+        <h1>This is a modal</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus, enim est eum excepturi explicabo
+          harum, ipsam iste iusto laudantium modi officia perspiciatis quae rem repudiandae vel velit voluptate.
+          Dolorem!</p>
+        <button @click="showModal = false">Hide modal</button>
+      </div>
+    </teleport>
   </div>
 </template>
 
@@ -30,5 +32,11 @@ const showModal = ref(false);
   background: beige;
   padding: 10px;
   color: #2c3e50;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 }
 </style>
