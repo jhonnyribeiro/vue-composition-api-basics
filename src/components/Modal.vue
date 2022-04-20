da671c291b3777e4c2ddb70955055e9e46328945
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-20 17:27:56 -0300

    Chield components

---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+  <teleport to="body">
+    <div class="modal">
+      <h1>This is a modal</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus, enim est eum excepturi explicabo
+        harum, ipsam iste iusto laudantium modi officia perspiciatis quae rem repudiandae vel velit voluptate.
+        Dolorem!</p>
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+
+<script>
+export default {
+  name: "Modal"
+}
+</script>
+
+
+<style scoped>
+.modal {
+  background: beige;
+  padding: 10px;
+  color: #2c3e50;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,20 +2,13 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <teleport to="body">
-      <div class="modal" v-if="showModal">
-        <h1>This is a modal</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus, enim est eum excepturi explicabo
-          harum, ipsam iste iusto laudantium modi officia perspiciatis quae rem repudiandae vel velit voluptate.
-          Dolorem!</p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
+    <Modal v-if="showModal"></Modal>
   </div>
 </template>
 
 <script setup>
 import {ref} from "vue";
+import Modal from "../components/Modal.vue";
 
 name: "ModalsView";
 
@@ -26,17 +19,3 @@ name: "ModalsView";
 const showModal = ref(false);
 
 </script>
-
-<style scoped>
-.modal {
-  background: beige;
-  padding: 10px;
-  color: #2c3e50;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
-</style>
