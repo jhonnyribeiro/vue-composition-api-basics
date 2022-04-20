868de75e2f58a64ee27193b042f9478fcfd31a44
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-20 15:54:30 -0300

    Compositon API - modal

---
+++ src/views/ModalsView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="modals">
+    <h1>Modals</h1>
+    <button @click="showModal = true">Show modal</button>
+    <div class="modal" v-if="showModal">
+      <h1>This is a modal</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus, enim est eum excepturi explicabo
+        harum, ipsam iste iusto laudantium modi officia perspiciatis quae rem repudiandae vel velit voluptate.
+        Dolorem!</p>
+      <button @click="showModal = false">Hide modal</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+name: "ModalsView";
+
+/**
+ * Modals
+ */
+
+const showModal = ref(false);
+
+</script>
+
+<style scoped>
+.modal {
+  background: beige;
+  padding: 10px;
+  color: #2c3e50;
+}
+</style>
