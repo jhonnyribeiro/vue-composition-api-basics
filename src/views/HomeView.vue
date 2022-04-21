dc910e9a962098e054f0370dfce7e02085fd8e21
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-20 22:09:28 -0300

    Create composable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -42,39 +42,7 @@ onMounted(() => {
 /**
  * Counter
  */
-const counterData = reactive({
-  count: 0,
-  title: 'My Object Counter'
-});
 
-watch(() => counterData.count, (newCount, oldCount) => {
-  if (newCount == 20) {
-    alert('You made it 20!! Go ahead... ')
-  }
-});
-
-const oddOrEven = computed(() => {
-  if (counterData.count % 2 === 0) {
-    return 'even';
-  } else {
-    return 'odd';
-  }
-});
-
-const increaseCounter = (amount, e) => {
-  counterData.count = counterData.count + amount;
-  nextTick(() => {
-    console.log("do something after dom had updated")
-  });
-};
-
-const decreaseCounter = (amount) => {
-  counterData.count = counterData.count - amount;
-};
-
-onMounted(() => {
-  console.log('Related to counter');
-});
 
 </script>
 
